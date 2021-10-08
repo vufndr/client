36eba07befc03061fe4f93dc65cd21a160f230c5
Richard McDaniel <richard@Richards-Personal-MacBook-Pro.local>
2021-10-07 20:02:05 -0500

    Dropbox redirect

---
+++ src/views/oauth/Dropbox.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: 'Dropbox',
+  mounted() {
+    console.log(this.$route.params);
+    this.$router.push({ name: 'settings' });
+  },
+}
+</script>

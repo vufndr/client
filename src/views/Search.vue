0aa2b038cb494c98f8ff4525388ac6a1286233b2
Richard McDaniel <richard@Richards-Personal-MacBook-Pro.local>
2021-10-25 17:00:07 -0500

    Facets

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,5 +1,20 @@
 <template>
   <div>
+  <ui-list role="group">
+    <template v-for="(item, index) in items">
+      <ui-item-divider v-if="item === '-'" :key="index"></ui-item-divider>
+      <ui-item v-else :key="index">
+        <ui-item-text-content>{{ item.text }}</ui-item-text-content>
+        <ui-item-last-content>
+          <ui-checkbox
+            v-model="checkedValues"
+            :value="item.value"
+            @click.native.stop
+          ></ui-checkbox>
+        </ui-item-last-content>
+      </ui-item>
+    </template>
+  </ui-list>
     <ui-grid>
       <ui-grid-cell columns="2" v-for="image in images" :key="image">
         <ui-card>
@@ -24,6 +39,13 @@ export default {
     return {
       facets: [],
       images: [],
+      items: [
+        {
+          text: '800x600',
+          value: '800x600',
+        },
+      ],
+      checkedValues: [],
     }
   },
   mounted() {

1464abc6a0f7d262d330ea1690cdddb3c43fd622
Richard McDaniel <richard@Richards-Personal-MacBook-Pro.local>
2021-10-27 19:53:10 -0500

    UI

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -7,7 +7,7 @@
         </ui-drawer-header>
         <ui-drawer-content>
           <div class="mt-2 ml-2 mr-2">
-            <ui-textfield fullwidth outlined with-leading-icon v-model="searches[name]" @change="search()">
+            <ui-textfield fullwidth outlined with-leading-icon v-model="searches[name]" @input="search()">
               Search
               <template #before>
                 <ui-textfield-icon>search</ui-textfield-icon>
